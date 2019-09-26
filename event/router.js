c7149f2fbf143b9893d159a87677c2ca53d94520
nirustanite <nirustanite@gmail.com>
2019-09-26 11:47:43 +0200

    Added route to CRUD operations of the event

--- event/router.js
+++ event/router.js
@@ -3,5 +3,50 @@ const Event = require('./model');
 
 const router = new Router();
 
+
+router.post('/event', (req, res, next) => {
+    Event.create(req.body)
+         .then(event => res.send(event))
+         .catch(next);
+});
+
+router.put('/event/:id', (req,res,next) => {
+    Event.findByPk(req.params.id)
+         .then(event => {
+             if(event){
+                return event.update(req.body)
+                      .then(event =>  res.send(event));
+             }
+             return res.status(404).end();
+         })
+         .catch(next)
+});
+
+router.get('/event/:id', (req,res,next) => {
+    Event.findByPk(req.params.id)
+         .then(event => {
+             if(event){
+                 return res.send(event);
+             }
+             return res.status(404).end();
+         })
+         .catch(next)
+});
+
+router.delete('/event/:id', (req,res,next) => {
+    Event.destroy({
+        where:{
+            id: req.params.id
+        }
+    })
+    .then(number => {
+        if(number){
+            return res.send({number})
+        }
+        return res.status(404).end();
+    })
+    .catch(next);
+});
+
 module.exports = router;
 
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-//const db = require('./db');
-const Event = require('./event/model');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const router = require('./event/router');
 
 const app = express();
-
 const port = 4000;
+const corsMiddleware = cors();
+const parserMiddleware = bodyParser.json();
+
+app.use(corsMiddleware);
+app.use(parserMiddleware);
+app.use(router);
 
 app.listen(port, () => console.log(`Starting on port ${port}`))
